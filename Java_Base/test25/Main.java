d34581100c0854e4bf290fc3cea1e83e34fafab1
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-03-18 16:17:37 +0800

    Delete Java_Base directory

--- Java_Base/test25/Main.java
+++
@@ -1,9 +0,0 @@
-package test25;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException{
-        Runtime.getRuntime().exec("shutdown -s -t 5");
-    }
-}
