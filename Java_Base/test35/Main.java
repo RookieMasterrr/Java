92bfe6dfab849bfd2616e613550089b9f59360c5
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-03-31 10:58:18 +0800

    Create Main.java

---
+++ Java_Base/test35/Main.java
@@ -0,0 +1,26 @@
+package test35;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new J_Thread(1));
+        Thread t2 = new Thread(new J_Thread(2));
+        Thread t3 = new Thread(new J_Thread(3));
+        t1.start();
+        t2.start();
+        t3.start();
+        
+    }
+}
+
+class J_Thread implements Runnable{
+    int id;
+    J_Thread(int ID){
+        id=ID;
+    }
+    @Override
+    public void run() {
+        for(int i=0; i<5; i++){
+            System.out.printf("id=%d ",id);
+        }
+}
+}
