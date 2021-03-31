9cb5f14886f4131dea52c118818896dccfbeb1af
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-03-31 20:21:10 +0800

    Update Main.java

--- Java_Base/test35/Main.java
+++ Java_Base/test35/Main.java
@@ -8,7 +8,7 @@ public class Main {
         t1.start();
         t2.start();
         t3.start();
-        
+        System.out.println("End");
     }
 }
 
@@ -18,9 +18,15 @@ class J_Thread implements Runnable{
         id=ID;
     }
     @Override
-    public void run() {
+    public void run(){
         for(int i=0; i<5; i++){
             System.out.printf("id=%d ",id);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+    
 }
 }
