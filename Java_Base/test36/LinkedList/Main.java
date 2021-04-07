197be3c4585170da1c8a7e21fa36b387622a93a5
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-07 23:24:10 +0800

    Update Main.java

--- Java_Base/test36/LinkedList/Main.java
+++ Java_Base/test36/LinkedList/Main.java
@@ -1,7 +1,16 @@
 package test36.LinkedList;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        LinkedList<Integer> aIntegers = new LinkedList<Integer>();
+        aIntegers.add(1);
+        aIntegers.add(2);
+        aIntegers.add(3);
+        System.out.println(aIntegers);
+        System.out.println(aIntegers.removeLast());
+        System.out.println(aIntegers.removeFirst());
+        System.out.println(aIntegers);
     }
 }
