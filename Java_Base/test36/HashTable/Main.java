9e1f53372811f40f070d78ec63a98867f0ee972a
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-08 19:43:10 +0800

    更新了HashTable

--- Java_Base/test36/HashTable/Main.java
+++ Java_Base/test36/HashTable/Main.java
@@ -1,7 +1,17 @@
 package test36.HashTable;
 
+import java.util.Hashtable;
+
 public class Main {
     public static void main(String[] args) {
+        Hashtable<Integer,String> aHashtable = new Hashtable<Integer,String>();
+        aHashtable.put(1, "a");
+        aHashtable.put(2, "b");
+        aHashtable.put(3, "c");
         
+        System.out.println(aHashtable);
+        System.out.println(aHashtable.entrySet());
+        System.out.println(aHashtable.elements());
+        System.out.println(aHashtable);
     }
 }
