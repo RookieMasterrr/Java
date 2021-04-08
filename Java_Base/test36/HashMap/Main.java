8f2f635a7809b45718166602d91bbbec481d1fe2
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-08 18:43:47 +0800

    更新了HashMap

--- Java_Base/test36/HashMap/Main.java
+++ Java_Base/test36/HashMap/Main.java
@@ -1,5 +1,15 @@
 package test36.HashMap;
 
+import java.util.HashMap;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        HashMap<Integer,String> aHashMap = new HashMap<Integer,String>();
+        // System.out.println(aHashMap.size());
+        aHashMap.put(1, "a");
+        aHashMap.put(2, "b");
+        System.out.println(aHashMap);
+        System.out.println(aHashMap.keySet());
+        System.out.println(aHashMap.values());
+    }
 }
