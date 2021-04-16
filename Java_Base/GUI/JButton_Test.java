8f99d68244d2f446234285e8c5cf0b288c26cc6d
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-16 15:53:19 +0800

    增加了JButton示例

--- Java_Base/GUI/JButton_Test.java
+++ Java_Base/GUI/JButton_Test.java
@@ -6,12 +6,15 @@ package GUI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.plaf.DimensionUIResource;
+
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 
-public class Main{
+public class JButton_Test{
     public static void main(String[] args) {
         JFrame aFrame = new JFrame("Hello");
         
@@ -20,21 +23,42 @@ public class Main{
         
         JPanel aJPanel = new JPanel();
         
-        JPanel bJPanel = new JPanel();
-        
-        JButton aButton = new JButton("But");
 
-        JLabel aJLabel = new JLabel("Lab");
+
+
+        JButton aButton = new JButton("But");
+        aButton.setPreferredSize(new DimensionUIResource(70, 70)); //一个设置按键大小的方法
+        
+        aButton.addActionListener(new ActionListener(){ // 使用匿名类实现接口
+            @Override
+            public void actionPerformed(ActionEvent e){
+                JButton tempButton =  (JButton)e.getSource();
+                tempButton.setText("寄!");
+            }
+        });
+
+        aButton.setEnabled(true);
+
+
+
+
 
         aJPanel.add(aButton);
-        bJPanel.add(aJLabel);
 
         aContainer.add(aJPanel,BorderLayout.SOUTH);
-        aContainer.add(bJPanel,BorderLayout.WEST);
         
         aFrame.setSize(300,300);
         aFrame.setDefaultCloseOperation(3);
         aFrame.setAlwaysOnTop(true);
         aFrame.setVisible(true);
     }
+}
+
+class bt1_action implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);        
+    }
+    
 }
