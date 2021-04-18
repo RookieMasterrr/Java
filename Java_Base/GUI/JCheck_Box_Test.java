16b7bbfc76d0919ae425ecdac2e984d5615205a0
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-18 23:55:16 +0800

    更新了checkbox

--- Java_Base/GUI/JCheck_Box_Test.java
+++ Java_Base/GUI/JCheck_Box_Test.java
@@ -2,7 +2,7 @@
 // 容器 Container
 // 面板 Panel 
 // 
-package GUI;
+// package GUI;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -12,10 +12,14 @@ import javax.swing.plaf.DimensionUIResource;
 
 import java.awt.Container;
 import java.awt.event.ActionEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 
-public class JButton_Test{
+
+public class JCheck_Box_Test{
     public static void main(String[] args) {
         JFrame aFrame = new JFrame("Hello");
         
@@ -24,8 +28,12 @@ public class JButton_Test{
         
         JPanel aJPanel = new JPanel();
 
-        JCheckBox aBox = new JCheckBox();
+        JCheckBox aBox = new JCheckBox("CheckBox1");
         
+        myListener aListener = new myListener();
+
+        aBox.addItemListener(aListener);
+
         aJPanel.add(aBox);
 
         aContainer.add(aJPanel,BorderLayout.SOUTH);
@@ -35,4 +43,20 @@ public class JButton_Test{
         aFrame.setAlwaysOnTop(true);
         aFrame.setVisible(true);
     }
+}
+
+class myListener implements ItemListener{
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        System.out.println("ddd");
+        System.out.println(e.getStateChange());
+        System.out.println(e.paramString());
+        if(e.getStateChange()==2){
+            System.out.println("no");
+        }else{
+            System.out.println("yes");
+        }
+    }
+
 }
