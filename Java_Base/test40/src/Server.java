d48cd1c258888ca005689fb188086da3ffbc4000
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-19 23:45:29 +0800

    更换为JTextArea

---
+++ Java_Base/test40/src/Server.java
@@ -0,0 +1,39 @@
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends ServerSocket{
+    public Server(int P)throws IOException{
+        super(P);
+    }
+    static public String Read(Socket aSocket)throws IOException{
+        byte a[] = new byte[65536];
+        InputStream aInputStream = aSocket.getInputStream();
+        aInputStream.read(a);
+        return (new String(a));
+    }
+    static public void Write(Socket aSocket) throws IOException{
+        OutputStream aOutputStream = aSocket.getOutputStream();
+        InputStream aInputStream = System.in;
+        byte a[] = new byte[65536];
+        aInputStream.read(a);
+        aOutputStream.write(a);
+    }
+    public static void main(String[] args) throws IOException{
+        Server aServer = new Server(5050);    
+        Socket serverSide = aServer.accept();    
+//***********************************************************//
+
+        while(true){
+            System.out.println(Read(serverSide));
+            Write(serverSide);
+        }
+
+//***********************************************************//
+        // serverSide.close();
+        // aServer.close();
+    }
+}
---
+++ Java_Base/test40/src/myGUI.java
@@ -0,0 +1,62 @@
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.plaf.DimensionUIResource;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+
+import java.awt.Container;
+import java.awt.event.ActionListener;
+public class myGUI {
+    static JFrame aFrame;
+    static Container aContainer;
+    static JPanel aJPanel;
+    static JTextArea EditArea;
+    static JTextArea RecArea;
+    static JButton aButton;
+    public myGUI(){
+        aFrame = new JFrame("aFrame");
+        aContainer = aFrame.getContentPane();
+        aContainer.setLayout(new FlowLayout());
+        aJPanel = new JPanel(); 
+
+        aJPanel.add(new JLabel("RecText"));
+
+        RecArea = new JTextArea();
+        RecArea.setPreferredSize(new DimensionUIResource(200, 100));
+        aJPanel.add(RecArea);
+
+        aJPanel.add(new JLabel("EditText"));
+
+        EditArea = new JTextArea();
+        EditArea.setPreferredSize(new DimensionUIResource(200, 100));
+        aJPanel.add(EditArea);
+
+        aButton = new JButton("aButton");
+        aButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println(EditArea.getText());
+                RecInfo(EditArea.getText());
+            }
+        });        
+        aJPanel.add(aButton);
+
+
+
+        aContainer.add(aJPanel);
+        aFrame.setSize(700,300);
+        aFrame.setDefaultCloseOperation(3);
+        aFrame.setVisible(true);
+    }
+
+    public static void RecInfo(String Info){
+        RecArea.setText(RecArea.getText()+"\n"+Info);
+    }
+    public static void main(String[] args) {
+        new myGUI();
+    }
+}   
