7d8a34237dc781cddc30254aa2140b479b9a57f6
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-21 22:39:39 +0800

    线程

--- Java_Base/test41/Client.java
+++ Java_Base/test41/Client.java
@@ -1,18 +1,49 @@
-// package test41;
+
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client extends Socket{
-    public Client(String host, int port) throws IOException{
-        super(host,port);
+    public static Client aClient;
+    public Client(String H,int P)throws IOException{
+        super(H,P);
+    }
+    public void Write(String aString)throws IOException{
+        OutputStream aOutputStream = this.getOutputStream();
+        aOutputStream.write(aString.getBytes());
+    }
+    public String Read() throws IOException{
+        InputStream aInputStream = this.getInputStream();
+        byte a[] = new byte[65536];
+        aInputStream.read(a);
+        return (new String(a));
     }
     public static void main(String[] args) throws IOException{
-        Client aClient = new Client("localhost", 5050);
-        
-        // OutputStream aOutputStream = aClient.getOutputStream();
-        // aOutputStream.write("Its Client".getBytes());
+        aClient = new Client("localhost", 5050);
+        // while(true){
+        //     aClient.Write("hello");
+        //     System.out.println(aClient.Read());
+        // }
+        }
+}
 
+
+class Cread implements Runnable{
+    @Override
+    public void run(){
+        while(true){
+            System.out.println(aC);
+        }
+    }
+}
+
+class Cwrite implements Runnable{
+    @Override
+    public void run(){
+        while(true){
+            Write(serverSide, "ServerEcho");
+        }
     }
 }
--- Java_Base/test41/Server.java
+++ Java_Base/test41/Server.java
@@ -1,18 +1,54 @@
-// package test41;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server extends ServerSocket{
-    public Server(int port) throws IOException{
-        super(port);
+    public static Server  aServer;
+    public Server(int P)throws IOException{
+        super(P);
     }
-    public static void main(String[] args) throws IOException {
-        Server aServer = new Server(5050);
+    static public String Read(Socket aSocket)throws IOException{
+        byte a[] = new byte[65536];
+        InputStream aInputStream = aSocket.getInputStream();
+        aInputStream.read(a);
+        return (new String(a));
+    }
+    static public void Write(Socket aSocket,String text) throws IOException{
+        OutputStream aOutputStream = aSocket.getOutputStream();
+        byte aText[] = text.getBytes();
+        aOutputStream.write(aText);
+    }
+    public static void main(String[] args) throws IOException{
+        aServer = new Server(5050);    
+        SConnect c1 = new Connect();
+        Sread r1 = new read();
+        Swrite w1 = new write();
+        c1.start();
+        r1.start();
+        w1.start();
+    }
+}
+
+class SConnect implements Runnable{
+    @Override
+    public void run(){
         while(true){
-            Socket aSocket = aServer.accept();
-            System.out.println(aSocket.getPort());
+            Socket serverSide = aServer.accept();      
+            new clientThread()
         }
+    }
+}
+
+class clientThread implements Runnable{
+    String host;
+    public clientThread(String H){
+        host = H;
+    }
+    @Override
+    public void run(){
+
     }
 }
