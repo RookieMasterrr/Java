0e0a65ef9d035681407b8f8749994d1887d474a5
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-21 19:58:38 +0800

    MutilConnect

--- Java_Base/test41/Client.java
+++ Java_Base/test41/Client.java
@@ -1,5 +1,18 @@
-package test41;
+// package test41;
 
-public class Client {
-    
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client extends Socket{
+    public Client(String host, int port) throws IOException{
+        super(host,port);
+    }
+    public static void main(String[] args) throws IOException{
+        Client aClient = new Client("localhost", 5050);
+        
+        // OutputStream aOutputStream = aClient.getOutputStream();
+        // aOutputStream.write("Its Client".getBytes());
+
+    }
 }
--- Java_Base/test41/Server.java
+++ Java_Base/test41/Server.java
@@ -1,5 +1,18 @@
-package test41;
+// package test41;
 
-public class Server {
-    
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends ServerSocket{
+    public Server(int port) throws IOException{
+        super(port);
+    }
+    public static void main(String[] args) throws IOException {
+        Server aServer = new Server(5050);
+        while(true){
+            Socket aSocket = aServer.accept();
+            System.out.println(aSocket.getPort());
+        }
+    }
 }
