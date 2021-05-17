3b877bdae43c1c50c71e68d7e3f680e25c66edd3
oFeasl <1033224672@qq.com>
2021-05-17 22:56:37 +0800

    jar

---
+++ Java_Base/test42/J_Hello.java
@@ -0,0 +1,9 @@
+public class J_Hello {
+    String name;
+    public J_Hello(String N){
+        this.name = N;
+    }
+    public String Hello(){
+        return "Hello" + this.name;
+    }    
+}
---
+++ Java_Base/test42/J_Test.java
@@ -0,0 +1,5 @@
+public class J_Test {
+    public static void main(String[] args) {
+        
+    }
+}
